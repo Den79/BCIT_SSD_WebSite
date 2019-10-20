e66ad6a1b4b07b90a2c568714fd3bdd95c8679eb
Den79 <test08081979@gmail.com>
2019-10-19 22:35:07 -0700

    month-div works correct

--- script/calendar.js
+++ script/calendar.js
@@ -1,4 +1,4 @@
-// READER from .txt file to array (calendarArrData)
+// *.TXT READER file --> array (calendarArrData)
 $(document).ready(function () {
     $.ajax({
         type: "GET",
@@ -25,18 +25,52 @@ function processData(allText) {
         }
     }
 
+
     // Insert the data from array (array calendarArrData) to code of the html page 
     // 
-    var htmlCode = '<div class="week">'; // opening the first week-div
-    var dayArr = ['Mon','Tues', 'Wed', 'Thur', 'Fri'];
+
+    var monthArrShortName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var monthIndex = 8; // first month of the calendar - Sep
+    var dayArr = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri'];
     var dayIndex = 0;
+    var htmlWeek = "";
+    var htmlMonth = "";
+    var finalHtmlCode = "";
+
     for (let i = 0; i < calendarArrData.length; i++) {
-        // If - filter to exclude empty cells
+
+        // If - filter to exclude the cells without data
         if (calendarArrData[i][0].includes("Week") ||
-            (calendarArrData[i][0].includes("Break"))) {
+            calendarArrData[i][0].includes("Break") ||
+            calendarArrData[i][0].includes("Industry Projects")) {
 
         } else {
-            htmlCode = htmlCode
+
+             // end of the week detector
+             if (dayIndex > 4) {
+                dayIndex = 0;
+                htmlMonth = htmlMonth + '<div class="week">' + htmlWeek + '</div>';
+                htmlWeek = "";
+            }
+
+            //end of the month detector
+            if (!(calendarArrData[i][0].includes(monthArrShortName[monthIndex]))) {
+                finalHtmlCode = finalHtmlCode
+                      + '<div class="month">'
+                    + monthArrShortName[monthIndex]
+                    + htmlMonth
+                    + '<div class="week">'
+                    + htmlWeek
+                    + '</div></div>';
+                htmlWeek = "";
+                htmlMonth = "";
+                monthIndex++;
+                if (monthIndex > 11) monthIndex = 0;
+            }
+
+
+            // days
+            htmlWeek = htmlWeek
                 + '<div class="day"><div class="dayOfWeek">'
                 + dayArr[dayIndex]
                 + '</div><div class="date">'
@@ -46,17 +80,21 @@ function processData(allText) {
                 + '</div><div class="instructor">'
                 + calendarArrData[i][2]
                 + '</div></div>';
-            dayIndex ++;
+            dayIndex++;
+  
         }
-    // end of the week detector
-     if (dayIndex > 4) {
-         dayIndex = 0;
-         htmlCode = htmlCode + '</div><div class="week">';
-       }  
-    }
-    htmlCode = htmlCode + '</div>' // close the last week - div
 
-    document.getElementById('day').innerHTML = htmlCode;
+    }
+    // add the last "week" and "month" - div
+    finalHtmlCode = finalHtmlCode
+                        + '<div class="month">'
+                        + monthArrShortName[monthIndex]
+                        + htmlMonth
+                        + '<div class="week">'
+                        + htmlWeek
+                        + '</div></div>';
+
+    document.getElementById('calendarBody').innerHTML = finalHtmlCode;
     //var d = new Date();
     //alert(d);
 
